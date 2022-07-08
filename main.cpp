57fdd46d03c94efacaa99a97f5c0ead9758284d1
weixian.zhou <weixian.zhou@ninebot.com>
2022-07-08 14:14:08 +0800

    完成demo窗口

--- main.cpp
+++ main.cpp
@@ -9,28 +9,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    Battery w;
-//    w.setWindowTitle("Battery");
-//    w.show();
-
-    QMainWindow w;
-    QVBoxLayout layout;
-    w.setLayout(&layout);
-
-//    QWifi qwifi;
-////    qwifi.setFixedSize(100, 100);
-////    layout.addWidget(&qwifi);
-//    qwifi.show();
-//    qwifi.setLevel(3);
-//    qwifi.setMaxLevel(5);
-//    QColor color = Qt::green;
-//    qwifi.setLevelColor(color);
-//    QColor color2 = Qt::white;
-//    qwifi.setLevelDefaultColor(color2);
-
-    QCharge qcharge;
-    qcharge.setGeometry(100,100,100, 100);
-    qcharge.show();
+    Battery w;
+    w.setWindowTitle("Battery");
+    w.show();
 
     return a.exec();
 }
